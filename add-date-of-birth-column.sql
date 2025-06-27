cf65e408dc3fb87e402c239e757f4aab2afa2999
Edgar Cabrera <edgar@trykeep.com>
2025-06-27 11:07:06 -0600

    Fix PR comments (node version and remove db comment)

--- add-date-of-birth-column.sql
+++ add-date-of-birth-column.sql
@@ -4,8 +4,5 @@
 ALTER TABLE applications
 ADD COLUMN IF NOT EXISTS date_of_birth DATE;
 
--- Add a comment to document the field
-COMMENT ON COLUMN applications.date_of_birth IS 'Date of birth for the applicant - collected in Step 7: Personal Information';
-
 -- Verify the column was added
 SELECT 'date_of_birth column added successfully to applications table' as status;
