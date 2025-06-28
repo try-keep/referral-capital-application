69dfae711bea4b3eb3559ffd1c8795d0764d74fe
Edgar Cabrera <edgar@trykeep.com>
2025-06-27 19:13:32 -0600

    [GRO-38] Move schema definition to supabase migrations

--- add_ip_address_column.sql
+++
@@ -1,6 +0,0 @@
--- Add ip_address column to applications table
-ALTER TABLE applications 
-ADD COLUMN ip_address TEXT;
-
--- Add a comment to document the column purpose
-COMMENT ON COLUMN applications.ip_address IS 'IP address of the user when they submitted the application for audit and compliance purposes';
