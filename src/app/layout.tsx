e48915ab4b205f9489ee4ef0153fb9ad49ea451b
Edgar Cabrera <edgar.cabrera@pm.me>
2025-06-30 09:31:09 -0600

    [GRO-33] Add segment tracking (#8)
    * [GRO-33] Add segment tracking
    
    * [GRO-33] Add supabase core files
    
    * [GRO-33] Empty commit
    
    ---------
    
    Co-authored-by: Edgar Cabrera <edgar@trykeep.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Script from 'next/script';
+import Analytics from '@/components/analytics';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -149,6 +150,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
       </body>
+      <Analytics />
     </html>
   );
 }
