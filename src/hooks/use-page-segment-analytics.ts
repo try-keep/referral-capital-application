1f4b02d8980f639a99c3622042610bd332579bc5
Cesar Pérez <cesaraugp01@gmail.com>
2025-07-02 15:39:34 -0500

    refactor(Analytics): :recycle: abstract getApplicationTraits into a utility and remove usage of useApplicationData

--- src/hooks/use-page-segment-analytics.ts
+++ src/hooks/use-page-segment-analytics.ts
@@ -1,9 +1,19 @@
 import { useEffect } from 'react';
-import { filterObjectEmptyValues, isDefined } from '@/utils';
 
 import { analytics } from '@/lib/segment';
-import { useApplicationData } from './';
 import { usePathname, useSearchParams } from 'next/navigation';
+import { getApplicationTraits } from '@/utils/analytics/traits';
+import { isDefined } from '@/utils';
+
+function extractApplicationData() {
+  const data = localStorage.getItem('referralApplicationData');
+  try {
+    return isDefined(data) ? JSON.parse(data) : {};
+  } catch (error) {
+    console.error('Error parsing application data', error);
+    return {};
+  }
+}
 
 /**
  * Hook to track page views and send application data to Segment
@@ -15,27 +25,17 @@ import { usePathname, useSearchParams } from 'next/navigation';
 export const usePageSegmentAnalytics = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const applicationData = useApplicationData();
+  const applicationData = extractApplicationData();
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
+    const handleRouteChange = (pathname: string) => {
       analytics.page(
         {
-          page: url,
+          page: pathname,
         },
         undefined,
         {
-          traits: {
-            ...(isDefined(applicationData)
-              ? filterObjectEmptyValues({
-                  email: applicationData.email,
-                  phone: applicationData.phone,
-                  firstName: applicationData.firstName,
-                  lastName: applicationData.lastName,
-                  businessName: applicationData.businessName,
-                })
-              : {}),
-          },
+          traits: getApplicationTraits(applicationData),
         }
       );
     };
@@ -45,20 +45,9 @@ export const usePageSegmentAnalytics = () => {
 
   //Track initial page view on page load when analytics is ready
   useEffect(() => {
-    console.log('analytics.ready', pathname);
     analytics.ready(() => {
       analytics.page({ page: pathname }, undefined, {
-        traits: {
-          ...(isDefined(applicationData)
-            ? filterObjectEmptyValues({
-                email: applicationData.email,
-                phone: applicationData.phone,
-                firstName: applicationData.firstName,
-                lastName: applicationData.lastName,
-                businessName: applicationData.businessName,
-              })
-            : {}),
-        },
+        traits: getApplicationTraits(applicationData),
       });
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
