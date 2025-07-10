2340319e4586a5b3641e708a3eaef4ea905bdd25
Edgar Cabrera <edgar.cabrera@pm.me>
2025-07-10 13:38:49 -0600

    Fix number of employees values
    Co-authored-by: Cesar Pérez <cesaraugp01@gmail.com>

--- src/components/forms/application/v2/number-of-employees.tsx
+++ src/components/forms/application/v2/number-of-employees.tsx
@@ -7,13 +7,13 @@ import { useApplicationStep } from '@/contexts/';
 import CardWithRadio from '../../fields/CardWithRadio';
 
 const numberOfEmployeesOptions = [
-  { value: 'Just me', label: 'Just me' },
-  { value: '2-5 employees', label: '2-5 employees' },
-  { value: '6-10 employees', label: '6-10 employees' },
-  { value: '11-25 employees', label: '11-25 employees' },
-  { value: '26-50 employees', label: '26-50 employees' },
-  { value: '51-100 employees', label: '51-100 employees' },
-  { value: 'Over 100 employees', label: 'Over 100 employees' },
+  { value: 'just-me', label: 'Just me' },
+  { value: '2-5', label: '2-5 employees' },
+  { value: '6-10', label: '6-10 employees' },
+  { value: '11-25', label: '11-25 employees' },
+  { value: '26-50', label: '26-50 employees' },
+  { value: '51-100', label: '51-100 employees' },
+  { value: 'over-100', label: 'Over 100 employees' },
 ];
 
 const NumberOfEmployees = () => {
